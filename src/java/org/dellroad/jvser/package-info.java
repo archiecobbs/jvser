be7d6cbdd7ded42423821a455b407dc423a48c5a
archie.cobbs <archie.cobbs@b41054e7-ca4e-aa42-1bb2-013bb36f098e>
2010-11-22 22:43:26 +0000

    Java doc improvements.

--- src/java/org/dellroad/jvser/package-info.java
+++ src/java/org/dellroad/jvser/package-info.java
@@ -6,6 +6,14 @@
  */
 
 /**
- * Top level Java package for the <a href="http://jvser.googlecode.com/">jvser</a> classes.
+ * Top level Java package for the <a href="http://jvser.googlecode.com/"><b>jvser</b></a> RFC 2217 client library.
+ *
+ * <p>
+ * The central class in this package is {@link org.dellroad.jvser.TelnetSerialPort}.
+ * </p>
+ *
+ * @see org.dellroad.jvser.TelnetSerialPort
+ * @see <a href="http://tools.ietf.org/html/rfc2217">RFC 2217</a>
+ * @see <a href="http://jvser.googlecode.com/">The <b>jvser</b> project home page</a>
  */
 package org.dellroad.jvser;
